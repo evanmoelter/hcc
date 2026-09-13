9b273a4d3d54607faa388be9688bf7e7a35d4761
Evan Moelter <2207348+evanmoelter@users.noreply.github.com>
2026-09-13 08:14:08 -0700

    feat(apollo): add Flux GitHub webhook

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -22,10 +22,14 @@ kubeconform_args=(
     "-verbose"
 )
 
+validate_manifests() {
+    sed 's/${SECRET_DOMAIN}/example.invalid/g' "$@" | kubeconform "${kubeconform_args[@]}"
+}
+
 echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
-    kubeconform "${kubeconform_args[@]}" "${file}"
+    validate_manifests "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
@@ -36,7 +40,7 @@ find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IF
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+      validate_manifests
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
@@ -47,7 +51,7 @@ find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IF
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+      validate_manifests
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
